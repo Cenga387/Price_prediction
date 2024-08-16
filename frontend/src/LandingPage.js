38bf7eefbff9aa80ee6f3e835f1c603b447702f9
Affan88888 <affankapidzic3@gmail.com>
2024-08-16 14:55:27 +0200

    Dodan FAQ i Footer

--- frontend/src/LandingPage.js
+++ frontend/src/LandingPage.js
@@ -31,14 +31,6 @@ export default function LandingPage() {
       <AppAppBar mode={mode} toggleColorMode={toggleColorMode} />
       <Hero />
       <Box sx={{ bgcolor: 'background.default' }}>
-        <LogoCollection />
-        <Features />
-        <Divider />
-        <Testimonials />
-        <Divider />
-        <Highlights />
-        <Divider />
-        <Pricing />
         <Divider />
         <FAQ />
         <Divider />
